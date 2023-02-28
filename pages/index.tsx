8f2d36fc783ce8aafee389a19c3d250024c0d0d5
nivascoder <121162928+codernivas@users.noreply.github.com>
2023-02-28 18:39:17 +0530

    Alert component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Greeting from "@/components/Greetings";
 import TodoList from "@/Projects/Todo/TodoComponent";
+import Alert from "@/components/Alert"
 
 export default function Home() {
  
   return (
     <>
-      <Greeting/>
+      <Alert severity={"error"}/>
     </>
   )
 }
