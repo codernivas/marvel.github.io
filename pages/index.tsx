a428aab00fd10750c2bd66ed199339b1804db006
nivascoder <121162928+codernivas@users.noreply.github.com>
2023-03-01 19:54:37 +0530

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,11 @@ import Alert from "@/components/Alert"
 import CustomButton from "@/components/ButtonCustom";
 import Stepper from "@/Projects/Stepper/StepperComponent";
 import PricingPage from "@/Projects/PricingPage";
-
 export default function Home() {
 
   return (
     <>
-      <Alert severity={"error"}/>
-      <CustomButton children={"hi"}/>
-      <PricingPage/>
+     <PricingPage/>
     </>
   )
 }
