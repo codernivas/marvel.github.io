73111f25f17a65a735065be7177618e0ed35bad6
nivascoder <121162928+codernivas@users.noreply.github.com>
2023-03-10 09:58:22 +0530

    fix 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import TodoList from "@/Projects/Todo/TodoComponent";
 import Alert from "@/components/Alert"
 import CustomButton from "@/components/ButtonCustom";
 import PricingPage from "@/Projects/PricingPage";
-import { LineChart } from "@/charts/LineChart";
+
 
 export default function Home() {
 
