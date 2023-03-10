a1a8de6136f21fe48ceff2841d8fa20ed1287aba
nivascoder <121162928+codernivas@users.noreply.github.com>
2023-03-10 09:34:41 +0530

    build error fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,13 @@ import Greeting from "@/components/Greetings";
 import TodoList from "@/Projects/Todo/TodoComponent";
 import Alert from "@/components/Alert"
 import CustomButton from "@/components/ButtonCustom";
-import Stepper from "@/Projects/Stepper/StepperComponent";
+
 import PricingPage from "@/Projects/PricingPage";
-import { LineChart } from "@/charts/LineChart";
+
 
 export default function Home() {
 
-  const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'My Dataset',
-        data: [
-          { x: 'January', y: 10 },
-          { x: 'February', y: 20 },
-          { x: 'March', y: 15 },
-          { x: 'April', y: 25 },
-          { x: 'May', y: 30 },
-          { x: 'June', y: 27 },
-          { x: 'July', y: 40 }
-        ]
-      }
-    ]
-  };
+ 
   return (
     <>
      <PricingPage/>
